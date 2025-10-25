5086b8673137dfc2771b3f8774f68a3d49504ce0
ngocanh0202 <buihuynhngocanh2020@gmail.com>
2025-10-25 22:06:27 +0700

    redo route

--- Front-end/ReadNest-FE/ReadNest-FE/Dictionaries/RouterManager.cs
+++ Front-end/ReadNest-FE/ReadNest-FE/Dictionaries/RouterManager.cs
@@ -6,18 +6,18 @@ namespace ReadNest_FE.Dictionaries
     {
         public static Dictionary<RouterType, string> routers = new Dictionary<RouterType, string>()
         {
-            { RouterType.HOME, "/" },
-            { RouterType.LOGIN, "/login" },
-            { RouterType.REGISTER, "/register" },
-            { RouterType.PROFILE, "/profile" },
-            { RouterType.IMAGES, "/images" },
-            { RouterType.FILTER, "/filter" },
-            { RouterType.CHAPTER, "/chapter/{id}" },
-            { RouterType.NOVEL, "/novel/{id}" },
-            { RouterType.POSTNOVEL, "/action/post-novel" },
-            { RouterType.PUTNOVEL, "/action/put-novel/{id}" },
-            { RouterType.POSTCHAPTER, "/action/post-chapter/{id}" },
-            { RouterType.UPDATECHAPTER, "/action/update-chapter/{id}" },
+            { RouterType.HOME, "/ReadNest/" },
+            { RouterType.LOGIN, "/ReadNest/login" },
+            { RouterType.REGISTER, "/ReadNest/register" },
+            { RouterType.PROFILE, "/ReadNest/profile" },
+            { RouterType.IMAGES, "/ReadNest/images" },
+            { RouterType.FILTER, "/ReadNest/filter" },
+            { RouterType.CHAPTER, "/ReadNest/chapter/{id}" },
+            { RouterType.NOVEL, "/ReadNest/novel/{id}" },
+            { RouterType.POSTNOVEL, "/ReadNest/action/post-novel" },
+            { RouterType.PUTNOVEL, "/ReadNest/action/put-novel/{id}" },
+            { RouterType.POSTCHAPTER, "/ReadNest/action/post-chapter/{id}" },
+            { RouterType.UPDATECHAPTER, "/ReadNest/action/update-chapter/{id}" },
             { RouterType.NONE, "#" }
         };
 
